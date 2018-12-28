95560b6024d99817f7a8371f8f78061c1df09d8c
Alessandro Scardova <alessandro.scardova@mahiz.it>
2018-12-28 18:58:08 +0100

    Closing inputStream

--- FatturaElettronica.Extensions/FatturaElettronicaSignedFileExtension.cs
+++ FatturaElettronica.Extensions/FatturaElettronicaSignedFileExtension.cs
@@ -48,6 +48,8 @@ namespace FatturaElettronica.Extensions
                 {
                     fattura.ReadXml(r);
                 }
+
+                inputStream.Close();
             }
         }
 

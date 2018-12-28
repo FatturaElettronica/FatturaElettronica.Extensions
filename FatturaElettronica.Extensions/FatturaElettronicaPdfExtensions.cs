0ecdda9883a19f910aa02ac0ac325704050bf87a
Alessandro Scardova <alessandro.scardova@mahiz.it>
2018-12-28 19:07:31 +0100

    Add new FatturaElettronicaPdfExtensions

---
+++ FatturaElettronica.Extensions/FatturaElettronicaPdfExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FatturaElettronica.Extensions
+{
+    public static class FatturaElettronicaPdfExtensions
+    {
+        public static void WritePdf(this Fattura fattura, string filePath, string xsltPath)
+        {
+
+        }
+    }
+}

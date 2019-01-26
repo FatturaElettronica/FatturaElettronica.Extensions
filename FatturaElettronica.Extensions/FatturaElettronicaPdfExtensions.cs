debdcc7576ef67370b0af8ddffd1698bec0e091f
Alessandro Scardova <alessandro.scardova@mahiz.it>
2019-01-26 10:46:35 +0100

    Add empty class PdfExtensions and Test

---
+++ FatturaElettronica.Extensions/FatturaElettronicaPdfExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FatturaElettronica.Extensions
+{
+    public static class FatturaElettronicaPdfExtensions
+    {
+
+    }
+}
---
+++ Test/FatturaElettronicaPdfExtensionsTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    [TestClass]
+    public class FatturaElettronicaPdfExtensionsTest
+    {
+    }
+}

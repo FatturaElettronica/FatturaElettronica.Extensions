50cac515418d60936a8351508f92ea83f71a1adb
Alessandro Scardova <alessandro@scardova.it>
2019-02-20 15:33:24 +0100

    DefaultFontProvider Set DefaultShipped = true

--- FatturaElettronica.Extensions/FatturaElettronicaPdfExtensions.cs
+++ FatturaElettronica.Extensions/FatturaElettronicaPdfExtensions.cs
@@ -103,7 +103,7 @@ namespace FatturaElettronica.Extensions
                         .SetBaseUri(".")
                         .SetCreateAcroForm(false)
                         .SetCssApplierFactory(new DefaultCssApplierFactory())
-                        .SetFontProvider(new DefaultFontProvider(true, false, false))
+                        .SetFontProvider(new DefaultFontProvider(true, true, false))
                         .SetMediaDeviceDescription(new MediaDeviceDescription(MediaType.PRINT))
                         .SetOutlineHandler(new OutlineHandler())
                         .SetTagWorkerFactory(new DefaultTagWorkerFactory());
